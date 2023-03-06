15a5a20aa67d7d2b8c8fad49f4cbff0230009823
Tyler Wang <tylerwang1996@gmail.com>
2023-03-06 15:02:57 -0500

    Update simulate.py
    working code

--- models/simulate.py
+++ models/simulate.py
@@ -3,18 +3,9 @@ import numpy as np
 
 
 class TimeSeriesSimulate:
-    def __init__(self, means, covar, years, steps_per_year, seed=None, sims=100):
-        """
-        A class for simulating time series using Geometric Brownian Motion.
+    def __init__(self, tickers, means, covar, years, steps_per_year, seed=None, sims=100):
 
-        Parameters:
-            means (list): A list of means for the Geometric Brownian Motion process.
-            covar (list of lists): A covariance matrix for the Geometric Brownian Motion process.
-            years (int): The number of years to simulate.
-            steps_per_year (int): The number of steps to take per year.
-            seed (int): (optional) The random seed to use for generating the normal variates.
-            sims (int): (optional) The number of simulations to run.
-        """
+        self.tickers = tickers
         self.means = means
         self.covar = covar
         self.years = years
@@ -22,29 +13,13 @@ class TimeSeriesSimulate:
         self.seed = seed
         self.sims = sims
 
-    def cholesky_decomp(self):
-        """
-        Performs a Cholesky decomposition of the covariance matrix.
+    def cholesky_decomp(self, covar):
 
-        Returns:
-            list of lists: The lower triangular Cholesky decomposition of the covariance matrix.
-        """
-        chol = np.linalg.cholesky(self.covar)
+        chol = np.linalg.cholesky(covar)
 
         return chol
 
     def simulate_gbm(self, mu=None, sigma=None):
-        """
-        Simulates Geometric Brownian Motion using the Euler-Maruyama method.
-
-        Parameters:
-            mu (float): The drift coefficient. If not specified, the first mean in the list of means is used.
-            sigma (float): The volatility coefficient. If not specified, the square root of the first element
-                           of the diagonal of the covariance matrix is used.
-
-        Returns:
-            np.ndarray: A numpy array of simulated stock prices of shape (n+1, M).
-        """
 
         # extract parameters from class attributes
         n = self.steps_per_year * self.years
@@ -80,22 +55,70 @@ class TimeSeriesSimulate:
 
         return St
 
+    def agg_simulation(self):
+
+        n_assets = len(self.means)
+
+        d_sim = {}
+        i = 0
+        while i < n_assets:
+            ticker = self.tickers[i]
+            mu = self.means[i]
+            sigma = np.sqrt(self.covar[i][i])
+            sim = self.simulate_gbm(mu, sigma)
+            df_sim = pd.DataFrame(sim)
+            d_sim[ticker] = df_sim
+            i += 1
+
+        dfs_sim = []
+        i = 0
+        chol = self.cholesky_decomp(self.covar)
+        while i < self.sims:
+            df_sim = pd.DataFrame(columns=self.tickers)
+            for col in df_sim:
+                df_sim[col] = d_sim[col][i]
+            df_sim = np.matmul(chol, df_sim.T).T
+            df_sim.set_axis(self.tickers, axis=1, inplace=True)
+            dfs_sim.append(df_sim)
+            i += 1
+
+        return dfs_sim
+
+
+def simulate_stock_prices_returns(prices_returns_df, years, steps_per_year, seed, sims):
+    # Calculate mean and covariance matrix
+    mean_returns = prices_returns_df.mean().to_numpy()
+    cov_matrix = prices_returns_df.cov().to_numpy()
+    print(mean_returns)
+    print(prices_returns_df.mean())
+
+    # Extract tickers from columns of prices_returns_df
+    tickers = list(prices_returns_df.columns)
+    print(tickers)
+
+    # Create an instance of the TimeSeriesSimulate class
+    ts = TimeSeriesSimulate(tickers, mean_returns, cov_matrix, years, steps_per_year, seed=seed, sims=sims)
+
+    # Simulate Geometric Brownian Motion
+    gbm = ts.agg_simulation()
+
+    return gbm
+
 
 if __name__ == "__main__":
-    means = [0.1, 0.05, 0.15]
-    covar = [[0.2, 0.1, 0.15],
-             [0.1, 0.3, 0.2],
-             [0.15, 0.2, 0.4]]
+    # create a random stock prices returns dataframe with three columns (tickers) and 100 rows (dates)
+    np.random.seed(123)
+    prices_returns_df = pd.DataFrame(np.random.randn(100, 3), columns=['A', 'B', 'C'])
+    prices_returns_df = prices_returns_df / 100
+
+    # set input parameters
     years = 5
     steps_per_year = 12
     seed = 123
-    sims = 100
+    sims = 2
 
-    # Create an instance of the TimeSeriesSimulate class
-    ts = TimeSeriesSimulate(means, covar, years, steps_per_year, seed=seed, sims=sims)
+    # call the simulate_stock_prices_returns function
+    gbm = simulate_stock_prices_returns(prices_returns_df, years, steps_per_year, seed, sims)
 
-    # Simulate Geometric Brownian Motion
-    gbm = ts.simulate_gbm()
-
-    # Display the results
+    # display the results
     print(gbm)
